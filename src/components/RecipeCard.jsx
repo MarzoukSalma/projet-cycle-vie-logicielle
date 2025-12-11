93e3b631bc611561aab69602af08e62f9e9f7cd8
Ihssane-fdi <ifairadi@outlook.com>
2025-12-11 12:03:16 +0100

    frontend-v1

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,4 +1,3 @@
-"use client"
 
 import { useState } from "react"
 import { Heart, MessageCircle, Share2, Bookmark } from "lucide-react"
--- src/components/RecipeFeed.jsx
+++ src/components/RecipeFeed.jsx
@@ -1,4 +1,4 @@
-"use client"
+
 
 import React from "react"
 import RecipeCard from "./RecipeCard"
