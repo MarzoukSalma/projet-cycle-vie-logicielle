e270ceead10d6c8b4c681b63bff8340d6401fafc
MarzoukSalma <salmamarzouk07@gmail.com>
2025-12-12 21:48:04 +0100

    base de donnes avec docker

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const userRoutes = require("./routes/user.routes");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware pour lire JSON
 app.use(express.json());
@@ -32,7 +32,3 @@ db.sequelize
   .catch((err) => {
     console.error("❌ Unable to connect to the database:", err);
   });
-
-
-
-  
